7a906054f6390cbc7abdd5cb637f9879dad071d6
Piyush Goel <piyushgoyaall@gmail.com>
2020-07-02 22:29:30 +0530

    Week 2 Solutions

---
+++ Week 2/4 Least Common Multiple/lcm.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+long long lcm_naive(int a, int b) {
+  for (long l = 1; l <= (long long) a * b; ++l)
+    if (l % a == 0 && l % b == 0)
+      return l;
+
+  return (long long) a * b;
+}
+
+int gcd_fast(int a, int b){
+	return b? gcd_fast(b,a%b):a;
+}
+
+long long lcm_fast(long long a, long long b)
+{
+  return a / gcd_fast(a, b) * b;
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  //std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
+  return 0;
+}
---
+++ Week 2/5 Fibonacci Number Again/fibonacci_huge.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+long long get_fibonacci_huge_naive(long long n, long long m) {
+    if (n <= 1)
+        return n;
+
+    long long previous = 0;
+    long long current  = 1;
+
+    for (long long i = 0; i < n - 1; ++i) {
+        long long tmp_previous = previous;
+        previous = current;
+        current = tmp_previous + current;
+    }
+
+    return current % m;
+}
+long long get_pisano_period(long long m) {
+    long long a = 0, b = 1, c = a + b;
+    for (int i = 0; i < m * m; i++) {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+        if (a == 0 && b == 1) 
+			return i + 1;
+    }
+}
+long long get_fibonacci_huge_fast(long long n, long long m) {
+
+	long long iterations = n % get_pisano_period(m);
+	if(n ==0 || n ==1)
+		return n;
+	long long a=0, b=1;
+	long long ans = iterations;
+	
+	for(long long i= 1; i < iterations ; i++){
+		ans = (a + b)%m;
+		a = b;
+		b = ans;
+	}
+    return ans%m;
+}
+
+int main() {
+    long long n, m;
+    std::cin >> n >> m;
+    //std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    std::cout << get_fibonacci_huge_fast(n, m) << '\n';
+    
+}
