b9388fbac0c8cd174ea28bffec48131dfcbaf5ef
Piyush Goel <piyushgoyaall@gmail.com>
2020-10-01 23:15:13 +0530

    Week 2

---
+++ Data Structures/Week 2/1_make_heap/build_heap.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std::swap;
+using std::pair;
+using std::make_pair;
+
+class HeapBuilder {
+ private:
+  vector<int> data_;
+  vector< pair<int, int> > swaps_;
+
+  void WriteResponse() const {
+    cout << swaps_.size() << "\n";
+    for (int i = 0; i < swaps_.size(); ++i) {
+      cout << swaps_[i].first << " " << swaps_[i].second << "\n";
+    }
+  }
+
+  void ReadData() {
+    int n;
+    cin >> n;
+    data_.resize(n);
+    for(int i = 0; i < n; ++i)
+      cin >> data_[i];
+  }
+  void minHeapify( int n, int i)
+  {
+    int smallest = i;   // Initialize smallest as root
+    int l = 2 * i + 1; // left = 2*i + 1
+    int r = 2 * i + 2; // right = 2*i + 2
+
+
+    if (l < n && data_[l] < data_[smallest])
+      smallest = l;
+
+    if (r < n && data_[r] < data_[smallest])
+      smallest = r;
+
+    if (smallest != i)
+    {
+      swap(data_[i], data_[smallest]);
+      swaps_.push_back(make_pair(i,smallest));
+      // Recursively heapify the affected sub-tree
+      minHeapify( n, smallest);
+    }
+  }
+  void buildHeap(){
+    int n = data_.size();   
+    for (int i = n / 2 - 1; i >= 0; i--)
+      minHeapify(n, i);
+  }
+
+  void GenerateSwaps() {
+    swaps_.clear();
+    // The following naive implementation just sorts 
+    // the given sequence using selection sort algorithm
+    // and saves the resulting sequence of swaps.
+    // This turns the given array into a heap, 
+    // but in the worst case gives a quadratic number of swaps.
+    //
+    // TODO: replace by a more efficient implementation
+    /*
+    for (int i = 0; i < data_.size(); ++i)
+      for (int j = i + 1; j < data_.size(); ++j) {
+        if (data_[i] > data_[j]) {
+          swap(data_[i], data_[j]);
+          swaps_.push_back(make_pair(i, j));
+        }
+      }
+      */
+    buildHeap();
+  }
+
+ public:
+  void Solve() {
+    ReadData();
+    GenerateSwaps();
+    WriteResponse();
+  }
+};
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  HeapBuilder heap_builder;
+  heap_builder.Solve();
+  return 0;
+}
